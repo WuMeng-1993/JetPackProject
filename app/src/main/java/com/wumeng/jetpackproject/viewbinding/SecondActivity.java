24f787cc015aee1648cef9611cf5bf209af252f7
WuMeng <541345568@qq.com>
2020-08-26 00:02:27 +0800

    添加dataBinding

---
+++ app/src/main/java/com/wumeng/jetpackproject/viewbinding/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.wumeng.jetpackproject.viewbinding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wumeng.jetpackproject.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
